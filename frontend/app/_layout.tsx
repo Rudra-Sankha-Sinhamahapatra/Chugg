2fae0fd493d1508ec193161a05c49b8328aa0bfb
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-04-25 21:46:27 +0530

    stack navigation

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,17 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import "../global.css";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return <GestureHandlerRootView>
-          <Slot />
-  </GestureHandlerRootView>
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(nobottombar)/accountinfo" options={{ headerShown: true, headerTitle: "Account Info", headerBackTitle:"Go Back" }} />
+      </Stack>
+    </GestureHandlerRootView>
+  );
 } 
